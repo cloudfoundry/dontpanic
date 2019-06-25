c2292c8d672d656b94edc3216fd8c285a318a148
Claudia Beresford <cberesford@pivotal.io>
2019-06-25 15:39:39 +0100

    Remove commented out code from CI scripts
    [#166605364]
    
    Co-authored-by: Giuseppe Capizzi <gcapizzi@pivotal.io>

--- ci/scripts/integration-tests.sh
+++ ci/scripts/integration-tests.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 set -eo pipefail
 
-# source "$( dirname "$BASH_SOURCE" )/test/utils.sh"
-
-# trap unmount_storage EXIT
-
-# mount_storage
-
-# make
-# make prefix=/usr/bin install
-
-# chmod +s /usr/bin/newuidmap
-# chmod +s /usr/bin/newgidmap
-
-# umask 077
-
 args=$@
 [ "$args" == "" ] && args="-r integration"
 ginkgo -mod vendor -p -nodes 5 -race $args
