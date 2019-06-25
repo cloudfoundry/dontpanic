18aac1bd51dc4f918ce491af60932903fc08a90f
Giuseppe Capizzi <gcapizzi@pivotal.io>
2019-06-25 15:36:30 +0100

    Move the CI tasks and scripts to GRR
    [#166605364]
    
    Co-authored-by: Claudia Beresford <cberesford@pivotal.io>

--- ci/scripts/integration-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-args=$@
-[ "$args" == "" ] && args="-r integration"
-ginkgo -mod vendor -p -nodes 5 -race $args
--- ci/scripts/unit-tests.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-
-args=$@
-[ "$args" == "" ] && args="-r"
-ginkgo -mod vendor -p -nodes 5 -race -skipPackage integration $args
